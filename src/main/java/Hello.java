98c454382e76b354e5429da9ad657e1ddc3c6485
yusifliilgar <efendiilgar@gmail.com>
2021-01-31 20:03:05 -0500

    movie 1 branch updated Hello.java

--- src/main/java/Hello.java
+++ src/main/java/Hello.java
@@ -1,7 +1,7 @@
 public class Hello {
 
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        System.out.println("Hello World, B21");
         System.out.println("B21 future SDET learning GIT");
     }
 
