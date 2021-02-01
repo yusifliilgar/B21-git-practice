b39558b02ce2438350b6265988b5bec4becb2b25
yusifliilgar <efendiilgar@gmail.com>
2021-01-31 20:57:30 -0500

    movie2 branch watching on the phone for 10 minutes

--- src/main/java/Hello.java
+++ src/main/java/Hello.java
@@ -3,6 +3,7 @@ public class Hello {
     public static void main(String[] args) {
         System.out.println("Hello World B22");
         System.out.println("B21 future SDET learning GIT");
+        // movie on the phone 10 minutes
     }
 
 
