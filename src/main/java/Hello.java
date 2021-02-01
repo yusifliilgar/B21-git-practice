22cb29620532f5fdb2e85f5fa8033b6dd496c083
yusifliilgar <efendiilgar@gmail.com>
2021-01-31 20:18:53 -0500

    movie branch final work is done, ready to merge not knowing spouse moved location of movie

--- src/main/java/Hello.java
+++ src/main/java/Hello.java
@@ -4,6 +4,7 @@ public class Hello {
         System.out.println("Hello World, B21");
         System.out.println("B21 future SDET learning GIT");
         // watching more movie in bedroom
+        // ready to go back to living room
     }
 
 
